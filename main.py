6914dcb35a53148689fe00ea2af72a475ae5fc23
Michael Paris <mlparis92@gmail.com>
2014-02-15 18:06:21 -0500

    Started adding grammar

--- main.py
+++ main.py
@@ -6,14 +6,124 @@ import logging
 from lexer import Lexer
 
 precedence = (
+	('nonassoc', 'larrow'),
+	('nonassoc', 'not'),
+	('nonassoc', 'equals', 'lt', 'le'),
     ('left', 'plus', 'minus'),
     ('left', 'times', 'divide'),
+    ('nonassoc', 'isvoid'),
+    ('right', 'tilde'),
+    ('nonassoc', 'at'),
+    ('nonassoc', 'dot')
 )
+
+########### Parser (token -> AST) #############
+
+from compiler import ast
+
+def lt_compare((left, right)):
+	return ast.Compare(left, [('<', right),])
+def le_compare((left, right)):
+	return ast.Compare(left, [('<=', right),])
+def eq_compare((left, right)):
+	return ast.Compare(left, [('==', right),])
+
+binary_ops = {
+	"plus": ast.Add,
+	"minus": ast.Sub,
+	"times": ast.Mul,
+	"divide": ast.Div,
+	"lt": lt_compare,
+	"le": le_compare,
+	"equals": eq_compare
+}
+
+# program : [class semi]+
+def p_program_one(p):
+	'''PROGRAM : CLASSDEF semi'''
+	p[0] = p[1]
+def p_program_many(p):
+	'''PROGRAM : PROGRAM CLASSDEF semi'''
+	p[0] = p[1] + p[2]
+
+def p_class_def(p):
+	'''CLASSDEF : class type lbrace rbrace
+				| class type lbrace FEATURE semi rbrace'''
+	pass
+
+def p_class_def_inherits(p):
+	'''CLASSDEF : class type inherits type lbrace rbrace
+				| class type inherits type lbrace FEATURE rbrace'''
+	pass
+
+def p_feature(p):
+	'''FEATURE : identifier lparen FORMAL rparen colon type lbrace EXPR rbrace
+				| identifier colon type
+				| identifier colon type larrow EXPR'''
+	pass
+
+def p_formal(p):
+	'''FORMAL : identifier colon type
+			| FORMAL comma identifier colon type
+			| '''
+	pass
+
+def p_expr(p):
+	'''EXPR : identifier larrow EXPR
+			| EXPR comma EXPR
+			| EXPR semi EXPR
+			| EXPR at type dot identifier lparen rparen
+			| EXPR dot identifier lparen rparen
+			| EXPR at type dot identifier lparen EXPR rparen
+			| EXPR dot identifier lparen EXPR rparen
+			| identifier lparen rparen
+			| identifier lparen EXPR rparen
+			| if EXPR then EXPR else EXPR fi
+			| while EXPR loop EXPR pool
+			| lbrace EXPR semi rbrace
+			| let identifier colon type in EXPR
+			| let identifier colon type larrow EXPR in EXPR
+			| case EXPR of identifier colon type rarrow EXPR semi esac
+			| lparen EXPR rparen
+			'''
+	pass
+
+def p_expr_doubles(p):
+	'''EXPR : new type
+			| isvoid EXPR
+			| tilde EXPR
+			| not EXPR'''
+	p[0] = (p[1], p[2])
+
+
+
+def p_expr_math(p):
+	'''EXPR : EXPR plus EXPR
+			| EXPR minus EXPR
+			| EXPR times EXPR
+			| EXPR divide EXPR
+			| EXPR lt EXPR
+			| EXPR le EXPR
+			| EXPR equals EXPR'''
+	# p[0] = binary_ops[p[2]]((p[1], p[3]))
+	p[0] = (p[1], p[2], p[3])
+
+
+def p_expr_terminal(p):
+	'''EXPR : identifier
+			| integer
+			| string
+			| true
+			| false'''
+	# p[0] = ast.Const(p[1])
+	p[0] = p[1]
+	
+
 
 def p_expression_plus(p):
     'expression : expression plus expression'
     print('expression : expression plus expression: {} + {}', p[1], p[3])
-    p[0] = (p[1], p[3])
+    p[0] = p[1] + p[3]
 
 def p_expression_num(p):
 	'expression : integer'
@@ -83,7 +193,7 @@ if __name__ == '__main__':
 
 		# Build the parser
 		parser = yacc.yacc(debug=True, debuglog = log)
-		print parser.productions
+		# print parser.productions
 
 		while True:
 			# try:
